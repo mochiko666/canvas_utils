ceef636c0abf37cd13e09336b7ca03d109036099
もちこ♥ <mochitarouji@gmail.com>
2026-01-17 18:36:56 +0900

    Fixed makeRGBA return value typo

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,4 +1,4 @@
 import type { RGBTuple } from "../types/color.js";
 
 export const makeRGB = ([r, g, b]: RGBTuple): string => `rgb(${r}, ${g}, ${b})`;
-export const makeRGBA = ([r, g, b]: RGBTuple, a: number): string => `rgb(${r}, ${g}, ${b}, ${a})`;
+export const makeRGBA = ([r, g, b]: RGBTuple, a: number): string => `rgba(${r}, ${g}, ${b}, ${a})`;
